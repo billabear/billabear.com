410fee6c6145043a5d0fe3dae8e20038f3a8bb31
Iain <iain@humblyarrogant.io>
2024-07-21 02:16:24 +0200

    Update blog

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -10,7 +10,7 @@
     </PageHero>
     <div class="mt-24 lg:mx-auto lg:max-w-7xl lg:px-8 relative lg:grid lg:grid-cols-4 ">
         <div class="col-span-3 mr-7">
-      <ContentList :query="{ ...query, skip: (page.value - 1) * postsPerPage }">
+      <ContentList :query="query">
         <template #default="{ list }">
           <div class="my-5 relative clear-both" v-for="article in list" :key="article._path">
             <NuxtLink :to="article._path" class="mb-5 text-3xl font-medium underline">{{ article.title }}</NuxtLink>
@@ -31,23 +31,21 @@
 
             <ul class="flex">
               <li class="p-2 page-item" v-if="page != 1 ">
-                <NuxtLink :to="{ path: '/blog', query: { page: page - 1 } }" class="page-link" aria-label="Previous">
+                <button @click="page--"   class="page-link" aria-label="Previous">
                   <span aria-hidden="true">&laquo;</span>
-                </NuxtLink>
+                </button>
               </li>
               <li class="p-2 page-item" v-for="n in totalPages" :key="n">
-                <NuxtLink :to="{ path: '/blog', query: { page: n } }" class="border rounded-lg p-2 font-medium hover:bg-gray-200" :class="{ active: n === page }" v-if="n !== page">{{ n }}</NuxtLink>
-                <span class="border rounded-lg p-2 text-gray-500" v-else>{{ n }}</span>
+                <button @click="page = n"  class="border rounded-lg p-2 font-medium hover:bg-gray-200" :class="{ active: n === page }" v-if="n !== page">{{ n }}</button>
+                <button disabled class="border rounded-lg p-2 text-gray-500" v-else>{{ n }}</button>
               </li>
-              <li class="p-2 page-item" :class="{ disabled: page === totalPages }">
-                <NuxtLink :to="{ path: '/blog', query: { page: page + 1 } }" class="page-link" aria-label="Next">
+              <li class="p-2 page-item" v-if="page !== totalPages">
+                <button @click="page++" class="page-link" aria-label="Next">
                   <span aria-hidden="true">&raquo;</span>
-                </NuxtLink>
+                </button>
               </li>
             </ul>
           </nav>
-
-          {{ page }}
         </div>
       <div>
         <BlogTop/>
@@ -57,13 +55,15 @@
 </template>
 
 <script setup lang="ts">
+
+import { ref, watch } from 'vue';
 import type { QueryBuilderParams } from "@nuxt/content";
 
 useHead({
   title: 'BillaBear - Blog',
 });
 const postsPerPage = 5;
-const page = ref(Number(useRoute().params.page) || 1); // Get page from route or default to 1
+const page = ref(Number(useRoute().query.page) || 1); // Get page from route or default to 1
 
 const skip = (page.value - 1) * postsPerPage; // Calculate skip based on page number
 const query: QueryBuilderParams = {
@@ -77,7 +77,9 @@ const query: QueryBuilderParams = {
 // Calculate total pages based on total articles count (optional)
 const totalArticles = await queryContent('blog').where({_draft: false}).count();
 const totalPages = Math.ceil(totalArticles / postsPerPage);
-
+watch(page, (newValue, oldValue) => {
+  window.location.search = '?page=' + newValue;
+})
 </script>
 
 <style scoped>
