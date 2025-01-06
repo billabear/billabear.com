7f470113afc086bf24b87c7f3d8c8fee0535f15d
Iain <iain@humblyarrogant.io>
2025-01-06 22:14:53 +0100

    Update calendar link

--- components/PageHero.vue
+++ components/PageHero.vue
@@ -9,7 +9,7 @@
         <slot name="subtitle"></slot>
       </h2>
       <div class="mt-12 text-center">
-        <a href="https://app.sessions.us/book/950e63e2-billabear---demo" target="_blank" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-teal-600 bg-white hover:bg-slate-100">{{ $t('pages.subscription_management.demo') }}</a>
+        <a href="https://calendly.com/iain-billabear/30min" target="_blank" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-teal-600 bg-white hover:bg-slate-100">{{ $t('pages.subscription_management.demo') }}</a>
         <a href="https://cloud.billabear.com/signup" target="_blank" class="border-black ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-black hover:bg-gray-900"> {{ $t('pages.subscription_management.free_trial') }} </a>
 
       </div>
