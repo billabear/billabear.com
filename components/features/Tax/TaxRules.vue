1c23c9285d6426631f41e022d823e88bb23e5cac
Iain <iain@humblyarrogant.io>
2025-01-23 15:09:41 +0100

    Image tax images

--- components/features/Tax/TaxRules.vue
+++ components/features/Tax/TaxRules.vue
@@ -23,9 +23,9 @@
         </div>
       </div>
     </div>
-    <div class="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
-      <div class="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-        <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none" src="/images/features/tax/tax_rules.png" :alt="$t('features.tax_rules.image_alt')">
+    <div class="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1 content-center">
+      <div class="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative">
+        <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 " src="/images/features/tax/tax_rules.png" :alt="$t('features.tax_rules.image_alt')">
       </div>
     </div>
   </div>
